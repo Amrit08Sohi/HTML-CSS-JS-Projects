46ca686ebc120f60c87975871dec6b523f21fcaf
Amrit08Sohi <amritpalsingh003963@gmail.com>
2023-08-01 23:09:28 +0530

    Adding todo list

---
+++ Todo List/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("Write Something")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+
+        let span = document.createElement("span"); 
+        span.innerHTML = "\u00d7";
+        li.append(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click",(e)=> {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+const saveData = () => {
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+const showList = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showList();
